3e18ecfb53a2f851cd382f6eee35e34f60777ec2
Jesusborjas006 <jesusbor006@gmail.com>
2024-11-27 16:32:21 -0600

    Add Wordpress Loop to iterate over each post content available

--- fictional-university-theme/index.php
+++ fictional-university-theme/index.php
@@ -1,1 +1,19 @@
-My first website!
+<?php 
+
+  // Checks to see if there are posts to display
+  if(have_posts()) :
+  // Loops through all available posts
+    while(have_posts()) : the_post(); // Displays current post data
+      ?>
+      <!-- Displays the title as a link -->
+        <h2><a href=<?php the_permalink() ?>><?php the_title(); ?></a></h2>
+        <!-- Displays post content -->
+        <p><?php the_content(); ?></p>
+      <?php
+  endwhile;
+// Shows message when post is not found
+else : echo "<p>Post not found</p>";
+  endif;  
+  
+
+?>
