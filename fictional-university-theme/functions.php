de6927516698d78314ea6cdf2ce9c6a87c6e684d
Jesusborjas006 <jesusbor006@gmail.com>
2024-12-03 12:53:01 -0600

    Import javascript for image slider in main page

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -5,6 +5,7 @@ function fictional_university_styles() {
   wp_enqueue_style("university-extra-styles", get_theme_file_uri("/build/index.css"));
   wp_enqueue_style("fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
   wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+  wp_enqueue_script("image-slider", get_theme_file_uri("/build/index.js"), ["jquery"], "1.0", true);
 }
 
 add_action("wp_enqueue_scripts", "fictional_university_styles");
