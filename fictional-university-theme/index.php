ff7dfce5dba7e41a1c4241baa81902da3d1d8dea
Jesusborjas006 <jesusbor006@gmail.com>
2024-12-03 12:10:10 -0600

    Add header and footer content to index.php file

--- fictional-university-theme/index.php
+++ fictional-university-theme/index.php
@@ -1,5 +1,7 @@
 <?php 
 
+  get_header();
+
   // Checks to see if there are posts to display
   if(have_posts()) :
   // Loops through all available posts
@@ -9,11 +11,12 @@
         <h2><a href=<?php the_permalink() ?>><?php the_title(); ?></a></h2>
         <!-- Displays post content -->
         <p><?php the_content(); ?></p>
+        <hr/>
       <?php
   endwhile;
 // Shows message when post is not found
 else : echo "<p>Post not found</p>";
   endif;  
   
-
+  get_footer();
 ?>
